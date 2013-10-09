14d386986d39a1abb67c299887f25d49d44f2d1d
Eva Kontogiannopoulou <evakont@gmail.com>
2013-10-09 12:37:44 +0300

    Create adjacency matrix

---
+++ src/edu/ntua/dblab/hecataeus/graph/visual/VisualCreateAdjMatrix.java
@@ -0,0 +1,49 @@
+package edu.ntua.dblab.hecataeus.graph.visual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ntua.dblab.hecataeus.graph.evolution.EdgeType;
+
+public class VisualCreateAdjMatrix {
+
+	private List<VisualNode> RQV;
+	
+	
+	public VisualCreateAdjMatrix(List<VisualNode> RQV){
+		
+		this.RQV = new ArrayList<VisualNode>(RQV);
+	}
+	
+	protected int[][] createAdjMatrix(){
+		System.out.println("----- creating adj matrix  -------");
+		int[][] adj = new int [this.RQV.size()][this.RQV.size()];
+		int pos = 0, k = 0;
+		for(int i = 0; i < this.RQV.size(); i++){
+			VisualNode v = this.RQV.get(i);
+			List<VisualEdge> outE = new ArrayList<VisualEdge>(v._outEdges);
+			List<VisualEdge> inE = new ArrayList<VisualEdge>(v._inEdges);
+			List<VisualEdge> allE = new ArrayList<VisualEdge>();
+			allE.addAll(outE);
+			allE.addAll(inE);
+			for(VisualEdge e : outE){
+				if(e.getType() == EdgeType.EDGE_TYPE_USES){	
+					VisualNode toNode = e.getToNode();
+					k = this.RQV.indexOf(v);
+					pos = this.RQV.indexOf(toNode);
+					adj[pos][k] = 1;
+					adj[k][pos] = 1;
+				}
+			}
+		}
+		for(int i = 0; i < this.RQV.size(); i++){
+			for(int j = 0; j < this.RQV.size(); j++){
+				if(adj[i][j] != 1){
+					adj[i][j] = 0;
+				}
+			}
+		}
+		System.out.println("----- done !!----------");
+		return adj;
+	}
+}
