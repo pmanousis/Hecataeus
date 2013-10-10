6e98717daa0a5a5fcf733e35753b013fa22ebe7d
Eva Kontogiannopoulou <evakont@gmail.com>
2013-10-10 18:47:12 +0300

    fix custom comparator

--- src/edu/ntua/dblab/hecataeus/graph/visual/ListComparator.java
+++ src/edu/ntua/dblab/hecataeus/graph/visual/ListComparator.java
@@ -1,18 +1,20 @@
 package edu.ntua.dblab.hecataeus.graph.visual;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class ListComparator implements Comparator <ArrayList<VisualNode>> {
 
 	@Override
 	public int compare(ArrayList<VisualNode> vert1, ArrayList<VisualNode> vert2) {
+		Collections.sort(vert1,new CustomComparator());
+		Collections.sort(vert2,new CustomComparator());
 		if(vert1.size() > vert2.size()){
-			return -1;
+			return 1;
 		}
-		else{
-			return 0;
-		}
+		return -1;
 	}
 
 }
