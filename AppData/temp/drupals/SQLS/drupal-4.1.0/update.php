c07f8baeed5272bb862aa2c941ff7cdb944d2f01
pmanousis <pmanousi@cs.uoi.gr>
2015-03-15 21:23:12 +0200

    Finished with drupal-4.1.0

--- AppData/temp/drupals/SQLS/drupal-4.1.0/update.php
+++ AppData/temp/drupals/SQLS/drupal-4.1.0/update.php
@@ -1,7 +1,7 @@
 SELECT nid,type FROM node WHERE type = 'story' OR type = 'page' OR type = 'blog' OR type = 'forum' OR type = 'book';
-SELECT * FROM $node->type WHERE nid = $node->nid;
+-- SELECT * FROM $node->type WHERE nid = $node->nid;
 SELECT n.nid, n.title FROM node n WHERE n.type = 'book' AND n.status = 0;
-SELECT uid, name, jabber, drupal FROM users WHERE jabber != '' || drupal != '';
+-- SELECT uid, name, jabber, drupal FROM users WHERE jabber != '' || drupal != '';
 SELECT rid, name FROM role;
 SELECT MAX(vid) AS high FROM vocabulary;
 SELECT * FROM collection;
@@ -9,7 +9,7 @@ SELECT * FROM tag;
 SELECT MAX(tid) AS high FROM term_data;
 SELECT nid,attributes FROM node WHERE attributes != '';
 SELECT name FROM tag WHERE attributes = '$node->attributes';
-SELECT * FROM variable WHERE value NOT LIKE 's';
+SELECT * FROM variable WHERE value <> 's';
 SELECT MAX(nid) FROM node;
 SELECT MAX(cid) FROM comments;
 SELECT MAX(tid) FROM term_data;
