115cd8295f6dd02cfbe135ab5bd0f840c9574b12
pmanousis <pmanousi@cs.uoi.gr>
2015-03-15 23:14:02 +0200

    Cleaned two more of the projects. Pending: 4.[4,5,6,7].* to complete. The good thing is it gets faster.

--- AppData/temp/drupals/SQLS/drupal-4.2.0/update.php
+++ AppData/temp/drupals/SQLS/drupal-4.2.0/update.php
@@ -1,5 +1,5 @@
 SELECT nid,type FROM node WHERE type = 'story' OR type = 'page' OR type = 'blog' OR type = 'forum' OR type = 'book';
-SELECT * FROM $node->type WHERE nid = $node->nid;
+-- SELECT * FROM $node->type WHERE nid = 0;
 SELECT n.nid, n.title FROM node n WHERE n.type = 'book' AND n.status = 0;
 SELECT rid, perm FROM role;
 SELECT rid, name FROM role;
@@ -9,7 +9,7 @@ SELECT * FROM tag;
 SELECT MAX(tid) AS high FROM term_data;
 SELECT nid,attributes FROM node WHERE attributes != '';
 SELECT name FROM tag WHERE attributes = '$node->attributes';
-SELECT * FROM variable WHERE value NOT LIKE 's'
+SELECT * FROM variable WHERE value <> 's';
 SELECT MAX(nid) FROM node;
 SELECT MAX(cid) FROM comments;
 SELECT MAX(tid) FROM term_data;
@@ -18,6 +18,6 @@ SELECT MAX(vid) FROM vocabulary;
 SELECT n.nid, t.tid FROM node n, term_node t WHERE n.nid = t.nid AND type = 'forum';
 SELECT MAX(bid) FROM bundle;
 SELECT MAX(fid) FROM feed;
-SELECT n.nid, c.cid, c.subject FROM node n LEFT JOIN comments c ON n.nid = c.nid WHERE c.comment LIKE '%s';
-SELECT nid, title FROM node WHERE teaser LIKE '%s' OR body LIKE '%s';
+SELECT n.nid, c.cid, c.subject FROM node n LEFT JOIN comments c ON n.nid = c.nid WHERE c.comment = '%s';
+SELECT nid, title FROM node WHERE teaser = '%s' OR body = '%s';
 SELECT rid, perm FROM role;
