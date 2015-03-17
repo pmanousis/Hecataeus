82711cd8ac32d2c7de5ee9966edff48e0a4b34ec
pmanousis <pmanousi@cs.uoi.gr>
2015-03-17 15:27:19 +0200

    Added views to all datasets.

--- AppData/temp/drupals/SQLS/drupal-4.1.0/node.php
+++ AppData/temp/drupals/SQLS/drupal-4.1.0/node.php
@@ -1,2 +1,2 @@
 SELECT nid FROM node WHERE title = '%s' AND status = 1;
-SELECT n.*, u.name, u.uid FROM node n LEFT JOIN users u ON n.uid = u.uid WHERE n.title = '%s' AND n.status = 1 ORDER BY created DESC;
+SELECT v.* FROM ourView v WHERE v.title = '%s' AND v.status = 1 ORDER BY created DESC;
